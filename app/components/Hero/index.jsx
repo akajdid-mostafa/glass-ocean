a5bd2622645adf35b7105d21b0b720c07163fcee
akajdid-mostafa <mostafaakajdid6@gmail.com>
2024-10-24 12:31:46 +0100

    mos

--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -59,21 +59,21 @@ const Banner = () => {
 
                 </div>
                 {/* SVG Wave */}
-                <div className="deca-custom-shape-divider" style={{ transform: 'rotate(180deg)', bottom: '-1px' }}>
-                    <svg
-                        data-name="Layer 1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 1200 120"
-                        preserveAspectRatio="none"
-                        style={{ width: 'calc(150% + 1.3px)', height: '90px' }}
-                    >
-                        <path
-                            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                            className="shape-fill"
-                            fill="#ffffff"
-                        ></path>
-                    </svg>
-                </div>
+                <div className="deca-custom-shape-divider" style={{ transform: 'rotate(180deg)', position: 'absolute', bottom: '0', left: '0', width: '100%' }}>
+                <svg
+                    data-name="Layer 1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1200 120"
+                    preserveAspectRatio="none"
+                    style={{ width: '100%', height: '50px' }}
+                >
+                    <path
+                        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                        className="shape-fill"
+                        fill="#ffffff"
+                    ></path>
+                </svg>
+            </div>
             </div>
         </div>
     );
