75fce6d048c7ee3aec9ed8c17c703da467cd2c44
akajdid-mostafa <mostafaakajdid6@gmail.com>
2024-10-28 14:33:29 +0100

    service

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from './components/Hero/index';
 import Features from './components/Work/index';
-import Cook from './components/Cook/index';
+import Cook from './components/About/index';
 import Expert from './components/Team/index';
 import Gallery from './components/Gallery/index';
 import Newsletter from './components/Newsletter/Newsletter';
 import CleaningServicesLayout from './components/Service/service';
+import ServicesSection from './components/Service/index';
 import SectionMap from './components/Section-Map/map';
 import VideoPopup from './components/YouTube/index';
 
@@ -12,6 +13,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <ServicesSection/>
       <CleaningServicesLayout/>
       <SectionMap/>
       <Features />
