bbb248031eb0543f9f8bb5044f102416be152ed8
mostafa <mostafaakajdid6@gmail.com>
2024-11-18 18:09:44 +0100

    mos

--- app/reparations-electriques-a-domicile/page.jsx
+++ app/reparations-electriques-a-domicile/page.jsx
@@ -3,13 +3,14 @@
 
 import Startpage from '../components/desService/index';
 import Place from '../components/desService/place';
-
+import Contact from "../components/Contact/index";
 
 
 export default function Simulat() {
     return (
       <main>
-      <Startpage
+        <Contact/>
+      {/* <Startpage
       backgroundImage="/images/simulation/facadee.jpg"
       mainHeading="Nettoyage de Façades pour Professionnels"
       subHeading="Nettoyage de façade"
@@ -17,7 +18,7 @@ export default function Simulat() {
       description="Les façades extérieures sont exposées à la saleté due aux intempéries et à la pollution. Lorsqu'on se rend dans un lieu pour le travail ou un rendez-vous, c'est l'une des premières choses que l'on remarque. Le nettoyage de vos façades est essentiel pour les embellir et réduire les risques de dégradation. Chez OceanConnecting, nos experts s'adaptent à vos besoins pour vous offrir des prestations fiables et de qualité."
       dividerColor="#ffffff" 
     />
-    <Place/>
+    <Place/> */}
       </main>
     )
   }
