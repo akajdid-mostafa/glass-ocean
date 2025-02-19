6d09727d9f6ab53c73096f6fd559511de433c090
Firezella <firezettasadiki1024@gmail.com>
2025-02-19 12:00:36 +0100

    improve SEO

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,27 @@
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/index';
+import { getMessages, getTranslations } from "next-intl/server";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/index";
 import { notFound } from "next/navigation";
 
-export const metadata = {
-  title: 'Ocean connecting Nettoyage',
-  description: "Nous sommes spécialisés dans le nettoyage et l'entretien",
-};
+export async function generateMetadata({
+  params,
+}: Readonly<{
+  params: { locale: string };
+}>) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: {
+      default: t("title"),
+      template: `%s | ${t("title")}`,
+    },
+    description: t("description"),
+    keywords: t("keywords"),
+  };
+}
 
 export default async function RootLayout({
   children,
@@ -19,21 +32,29 @@ export default async function RootLayout({
 }>) {
   const messages = await getMessages();
 
-  const validLocales = ["fr","en", "ar"];
+  const validLocales = ["fr", "en", "ar"];
   if (!validLocales.includes(locale)) {
     notFound();
   }
 
   const direction = locale === "ar" ? "rtl" : "ltr";
 
-  
   return (
     <html lang={locale} dir={direction}>
       <head>
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <meta name="apple-mobile-web-app-title" content="MyWebSite" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
